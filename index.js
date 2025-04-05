4b66b2dc33dcb6cf685dba3224d2e665b1e3915f
Dante <lostintokyoto@icloud.com>
2025-04-04 23:21:07 -0300

    fix: usar porta do Render

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-// Serve static files from the root directory
 app.use(express.static(path.join(__dirname)));
 
-// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-}); 
+});
